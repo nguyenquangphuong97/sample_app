f34194f908032c203cb7a83e49b06b9824f95f8d
nguyenquangphuong97 <nqputcung97@gmail.com>
2019-05-26 20:16:34 -0700

    chapter 5: filling in the layout

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
 class ActiveSupport::TestCase
   fixtures :all
-
-  # Add more helper methods to be used by all tests here...
+  include ApplicationHelper
 end
