92a81da112d031101680137038b25824f0486e68
nguyenquangphuong97 <nqputcung97@gmail.com>
2019-05-30 01:39:36 -0700

    chapter 9: Advance login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate params[:session][:password]
       log_in user
+      remember_check params[:session][:remember_me], user
       redirect_to user
     else
       flash.now[:danger] = t "errors.msg1"
@@ -13,7 +14,13 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_path
+  end
+
+  private
+
+  def remember_check checked, user
+    checked == Settings.checked ? remember(user) : forget(user)
   end
 end
