af4c52b92f89f955630fa6592bf208f97d5055fd
nguyenquangphuong97 <nqputcung97@gmail.com>
2019-06-02 21:09:41 -0700

    chapter 14: Following Users

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module SampleApp
       'locales', '**', '*.{rb,yml}')]
     config.i18n.available_locales = [:en, :vi]
     config.i18n.default_locale = :en
+    config.action_view.embed_authenticity_token_in_remote_forms = true
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
